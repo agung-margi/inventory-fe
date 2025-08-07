aed29a4a967e3aec61535ae22d060d711829049b
shofwan-14 <muhammadshofwankhaidar@gmail.com>
2025-08-07 22:08:07 +0700

    feat: add page item, warehouse, user

--- src/components/form/create-form/CreateWarehouseComponents.tsx
+++ src/components/form/create-form/CreateWarehouseComponents.tsx
@@ -10,32 +10,22 @@ import { useNavigate } from "react-router";
 import Button from "../../ui/button/Button.tsx";
 
 export default function CreateWarehouseComponents() {
-  const [showPassword, setShowPassword] = useState(false);
+  const [nama, setNama] = useState("");
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [alamat, setAlamat] = useState("");
   const navigate = useNavigate();
 
-  const roles = [
-    { value: "admin", label: "Admin" },
-    { value: "superadmin", label: "Superadmin" },
-  ];
-  const handleSelectChange = (value: string) => {
-    console.log("Selected value:", value);
-  };
+
+
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // Ensure CSRF token is fetched before login/registerfetchCsrfToken
+    
 
-    if (!email || !password) {
-      alert("Email and password are required");
-      return;
-    }
-
-    await register(email, password);
-    navigate("/products");
+    // await register(nama, alamat);
+     navigate("/warehouses");
+    console.log(nama,alamat)
   };
 
   return (
@@ -44,16 +34,17 @@ export default function CreateWarehouseComponents() {
         <form onSubmit={handleSubmit}>
           <div>
             <Label htmlFor="input">Nama Warehouse</Label>
-            <Input type="text" id="name" placeholder="Masukan Nama Warehouse" />
+            <Input type="text" id="name" placeholder="Masukan Nama Warehouse" value={nama}
+              onChange={(e) => setNama(e.target.value)} />
           </div>
           <div>
             <Label htmlFor="input">Alamat</Label>
             <Input
               type="text"
-              id="email"
-              placeholder="example@mail.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              id="alamat"
+              placeholder="example kawasan hyundai no 31"
+              value={alamat}
+              onChange={(e) => setAlamat(e.target.value)}
             />
           </div>
           <div className="flex justify-end mt-4 space-x-2">
