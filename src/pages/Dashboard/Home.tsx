dfae8624eac965d0196b2a0c21764c736a4e4f80
agung-margi <agung.margyono@gmail.com>
2025-08-15 13:35:25 +0700

    feat: sidebar ok

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       />
       <div className="grid grid-cols-1 gap-4 md:gap-6">
         <div className="col-span-12 space-y-6 xl:col-span-7">
-          <EcommerceMetrics />
+          {/* <EcommerceMetrics /> */}
         </div>
       </div>
     </>
